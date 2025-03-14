70442575482b7ace8e76b93d1fb939516157ba9a
brody-brody <143447819+brody-brody@users.noreply.github.com>
2025-03-14 02:43:38 +0000

    Deploying to gh-pages from @ brody-brody/brody-brody.github.io@291d9542533e9f2deee9f057f7d5d88e48e6c07f 🚀

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ document.querySelector('.contact-form').addEventListener('submit', function (eve
 
 
     Email.send({
-        Host: "smtp.elasticemail.com",
+        Host: "s1.maildns.net",
         Username: "tgqjlalo",
         Password: "-wI*xztEB9K442",
         To: 'brodysilva.dev@gmail.com',
