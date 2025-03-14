fcf9ee24e8ea9f1a4c284c839db967aabb3fd88d
brody-brody <143447819+brody-brody@users.noreply.github.com>
2025-03-14 01:34:24 +0000

    Deploying to gh-pages from @ brody-brody/brody-brody.github.io@4eff00aec27c4a0bf369053525049d717fe55503 🚀

--- script.js
+++ script.js
@@ -84,7 +84,9 @@ document.querySelector('.contact-form').addEventListener('submit', function (eve
     const message = document.querySelector('textarea[name="message"]').value;
 
     Email.send({
-        SecureToken: "ae170b0f-e907-478c-bc0d-2cfe5d6fb3e2",
+        Host: "HOST_TOKEN",
+        Username: "USER_TOKEN",
+        Password: "PASS_TOKEN",
         To: 'brodysilva.dev@gmail.com',
         From: email,
         Subject: `New message from ${name}`,
